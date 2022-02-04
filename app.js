a9b34fc3b33cff0c27986b9f94ccd44da722cb40
Abdülmuttalib GÜLER <64988496+TalibGuler@users.noreply.github.com>
2022-02-04 22:15:57 +0300

    nodemon eklendi

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-const fn = function () {
-  console.log('Function working');
-};
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('MERHABA');
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı`);
+});
