603a988aba8ce5934b72e4f9f59b9a2db98314ee
Abdülmuttalib GÜLER <64988496+TalibGuler@users.noreply.github.com>
2022-02-05 18:06:53 +0300

    Static Files eklendi

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
+const myLogger = (req, res, next) => {
+  console.log('Middleware log 1');
+  next();
+};
+
+const myLogger2 = (req, res, next) => {
+  console.log('Middleware log 2');
+  next();
+};
+
+//MIDDLEWARES
+app.use(express.static('public'));
+app.use(myLogger);
+app.use(myLogger2);
+
 app.get('/', (req, res) => {
-  res.send('MERHABA');
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
 });
 
 const port = 3000;
