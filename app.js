948eb5c4d8781a70db6662dd377b6f9f4755718c
Abdülmuttalib GÜLER <64988496+TalibGuler@users.noreply.github.com>
2022-02-05 18:44:53 +0300

    template engine ejs eklendi

--- app.js
+++ app.js
@@ -1,26 +1,34 @@
 const express = require('express');
+const ejs = require('ejs');
 const path = require('path');
 
 const app = express();
 
-const myLogger = (req, res, next) => {
-  console.log('Middleware log 1');
-  next();
-};
+// TEMPLATE ENGINE
+app.set("view engine","ejs") // template engine olarak ejs kullanacağımızı söylüyoruz
 
-const myLogger2 = (req, res, next) => {
-  console.log('Middleware log 2');
-  next();
-};
+// const myLogger = (req, res, next) => {
+//   console.log('Middleware log 1');
+//   next();
+// };
 
 //MIDDLEWARES
 app.use(express.static('public'));
-app.use(myLogger);
-app.use(myLogger2);
+// app.use(myLogger);
 
+//ROUTES
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  //res.sendFile(path.resolve(__dirname, 'temp/index.html')); // dosya gönderme şekli
+  res.render('index')
 });
+
+app.get('/about', (req,res)=> {
+  res.render('about')
+})
+
+app.get('/add', (req,res)=> {
+  res.render('add')
+})
 
 const port = 3000;
 
